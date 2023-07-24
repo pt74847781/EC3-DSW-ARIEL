9b9f3dc43d05650652cf41d52694fa183bab2ad4
pt74847781 <134320456+pt74847781@users.noreply.github.com>
2023-07-24 16:58:21 +0000

    primercommit

---
+++ src/main/java/com/example/EC3dsw/Ec3DswApplication.java
@@ -0,0 +1,13 @@
+package com.example.EC3dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3DswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3DswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EC3dsw/Ec3DswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EC3dsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3DswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
