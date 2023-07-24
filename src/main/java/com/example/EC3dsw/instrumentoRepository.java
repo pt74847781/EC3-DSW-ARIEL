365fdb2a7dc2ac1939124e5698786aa3c044dbf7
pt74847781 <134320456+pt74847781@users.noreply.github.com>
2023-07-24 18:32:10 +0000

    cuartocommit

--- src/main/java/com/example/EC3dsw/instrumentoRepository.java
+++ src/main/java/com/example/EC3dsw/instrumentoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "/api/ec3/", path = "categorias")
+@RepositoryRestResource(collectionResourceRel = "/api/ec3", path = "instrumentos")
 public interface instrumentoRepository extends PagingAndSortingRepository<instrumento, Integer>,CrudRepository<instrumento, Integer> {
     
     List<instrumento> findByNombre(@Param("nombre") String nombre);
